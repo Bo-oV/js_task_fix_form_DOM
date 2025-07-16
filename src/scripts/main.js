fba294d928370210de089be04cc077d1b9576801
Pavlo <marlana08022022@gmail.com>
2025-07-16 22:01:07 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const form = document.querySelectorAll('form');
+
+const iputs1 = form[0].querySelectorAll('input');
+const iputs2 = form[1].querySelectorAll('input');
+
+const inputs = [...iputs1, ...iputs2];
+
+inputs.forEach((input, index) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+  input.parentNode.insertBefore(label, input);
+
+  input.placeholder =
+    input.name.charAt(0).toLocaleUpperCase() + input.name.slice(1);
+});
